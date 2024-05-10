52a9a0353f904cceca4629cd4196270039d5b9c2
Agrawal, Jyotsna <122946262+jyotsnaagrawal@users.noreply.github.com>
2024-05-10 15:31:45 -0500

    Using Grid and Recycler View

--- app/src/main/java/com/jyotsna/androidrecyclerviewandcardview/MainActivity.kt
+++ app/src/main/java/com/jyotsna/androidrecyclerviewandcardview/MainActivity.kt
@@ -16,8 +16,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 2) // Set the grid layout with 2 columns
         val imageList = listOf(drawable.rainy, drawable.sunny, drawable.sunset,drawable.ship, drawable.seass, drawable.winter,drawable.rainy, drawable.sea, drawable.seas,drawable.rainy,
             drawable.thunderstorm, drawable.woman, )
-        val captionList = listOf("Caption 1", "Caption 2", "Caption 3","Caption 4", "Caption 5", "Caption 6" ,
-            "Caption 7", "Caption 8", "Caption 9")
+        val captionList = listOf("Rainy", "Sunny", "Sunset","Ship", "sea", "Rainy", "Sunny", "Sunset","Ship", "sea")
         recyclerView.adapter = MyAdapter(imageList, captionList)
     }
 }
